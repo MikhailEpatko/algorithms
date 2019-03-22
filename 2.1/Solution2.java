fd6c3a041cbe0fa057241755190c3004258a6b04
Mikhail Epatko <mikhail.epatko@paragon-software.com>
2019-03-22 16:45:59 +0300

    Add non recursive merge-sorting

--- 2.1/Solution2.java
+++ 2.1/Solution2.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Solution2 {
 
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Solution2 {
             for (int i = 0; i < nums.length; i++) {
                 nums[i] = Integer.parseInt(tokens[i]);
             }
-            nums = mergeSort(nums);
+            mergeSort(nums);
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < nums.length; i++) {
                 sb.append(nums[i]);
@@ -30,14 +31,42 @@ public class Solution2 {
         }
     }
 
-    private static int[] mergeSort(int[] array) {
-        for (int size = 1; size <= array.length; size *= 2) {
-
+    private static void mergeSort(int[] array) {
+        int len = array.length;
+        int n = 1;
+        int shift;
+        int two_size;
+        int[] arr2;
+        while (n < len) {
+            shift = 0;
+            while (shift < len) {
+                if (shift + n >= len) {
+                    break;
+                }
+                two_size = (shift + n * 2 > len) ? (len - (shift + n)) : n;
+                arr2 = merge(Arrays.copyOfRange(array, shift, shift + n),
+                        Arrays.copyOfRange(array, shift + n, shift + n + two_size));
+                if (n + two_size >= 0) {
+                    System.arraycopy(arr2, 0, array, shift, n + two_size);
+                }
+                shift += n * 2;
+            }
+            n *= 2;
         }
     }
 
     private static int[] merge(int[] left, int[] right) {
-
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] result = new int[left.length + right.length];
+        while (i < left.length || j < right.length) {
+            if (i == left.length || (j < right.length && right[j] < left[i])) {
+                result[k++] = right[j++];
+            } else {
+                result[k++] = left[i++];
+            }
+        }
         return result;
     }
 }
